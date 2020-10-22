5aadb9ee4b872ee2d2e0142f73adc042bb00f3cc
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-22 08:36:54 +0100

    Added response from server on endpoint access.

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.get("/", (req,res));
+app.get("/", (req,res) => res.json({ message: "Hello World"}));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
