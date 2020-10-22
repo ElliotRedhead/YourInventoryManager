d86557cfc16a3915aaf0188ec219bd9fb8d7eee8
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-22 08:39:18 +0100

    Made server link easily accessible from terminal.

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const port = 3000;
 app.use(express.json());
 app.get("/", (req,res) => res.json({ message: "Hello World"}));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`));
