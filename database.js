06303615e71083cb42ac296768d28eac80bda7b8
elliot.redhead <elliot.redhead@gmail.com>
2020-10-24 19:40:24 +0100

    Database access based on environment.

--- database.js
+++ database.js
@@ -1,17 +1,28 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_SCHEMA || 'postgres',
-                                process.env.DB_USER || 'postgres',
-                                process.env.DB_PASSWORD || '',
-                                {
-                                    host: process.env.DB_HOST || 'localhost',
-                                    port: process.env.DB_PORT || 5432,
-                                    dialect: 'postgres',
-                                    dialectOptions: {
-                                        ssl: process.env.DB_SSL == "true"
-                                    }
-                                });
+if (process.env.ENVIRONMENT == "development"){
+  const sequelize = new Sequelize(
+    process.env.DB_SCHEMA || 'postgres',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || '',
+    {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: process.env.DB_SSL == "true"
+        }
+    });
+} else if (process.env.ENVIRONMENT == "production"){
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
+}
 
 const Person = sequelize.define('Person', {
     firstName: {
