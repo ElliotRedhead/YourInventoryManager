cef3bb3f830da4769eb6e5573420996444bad526
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-24 23:54:04 +0100

    Sequelize config edit.

--- database.js
+++ database.js
@@ -2,18 +2,17 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL || "",
-  process.env.DB_SCHEMA || 'postgres',
-  process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD || '',
-  {
-      host: process.env.DB_HOST || 'localhost',
-      port: process.env.DB_PORT || 5432,
-      dialect: 'postgres',
-      dialectOptions: {
-          ssl: process.env.DB_SSL == "true"
-      }
-  });
+  process.env.DATABASE_URL || "postgres",
+                                process.env.DB_USER || 'postgres',
+                                process.env.DB_PASSWORD || '',
+                                {
+                                    host: process.env.DB_HOST || 'localhost',
+                                    port: process.env.DB_PORT || 5432,
+                                    dialect: 'postgres',
+                                    dialectOptions: {
+                                        ssl: process.env.DB_SSL == "true"
+                                    }
+                                });
 
 const Person = sequelize.define('Person', {
     firstName: {
