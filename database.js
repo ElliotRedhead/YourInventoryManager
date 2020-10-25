87e49ed4429c7899092000808cb8e60796b30ac5
ElliotRedhead <elliot.redhead@gmail.com>
2020-10-25 08:58:18 +0000

    Troubleshooting sequelize.

--- database.js
+++ database.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || "postgres",
-                                process.env.DB_USER || 'postgres',
-                                process.env.DB_PASSWORD || '',
-                                {
-                                    host: process.env.DB_HOST || 'localhost',
-                                    port: process.env.DB_PORT || 5432,
-                                    dialect: 'postgres',
-                                    dialectOptions: {
-                                        ssl: process.env.DB_SSL == "true"
-                                    }
-                                });
+const sequelize = process.env.DATABASE_URL ? new Sequelize(
+  process.env.DATABASE_URL,
+  {
+      dialect: 'postgres',
+      dialectOptions: {
+          ssl: true
+      }
+    }
+  ) : new Sequelize(
+    "postgres",
+    "password",
+    {
+      host:'localhost',
+      port:5432,
+      dialect:"postgres",
+      dialectOptions: {
+        ssl: process.env.DB_SSL == "true"
+      }
+    }
+  )
+                            
 
 const Person = sequelize.define('Person', {
     firstName: {
@@ -26,6 +35,6 @@ const Person = sequelize.define('Person', {
 });
 
 module.exports = {
-    sequelize: sequelize,
-    Person: Person
+  sequelize: sequelize,
+  Person: Person
 };
