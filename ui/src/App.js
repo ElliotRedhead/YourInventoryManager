297047a3e396004e0c3d5664bd7bac151dda2bcc
ElliotRedhead <elliot.redhead@gmail.com>
2020-11-07 17:39:24 +0000

    Added routing: authentication & inventory pages.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,37 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Authentication from "./authentication/Authentication";
+import Inventory from "./inventory/Inventory";
+
 
 const App = () => {
-	// Products list state management
-	const [productsList, setProductsList] = useState("");
-	
-	const fetchProducts = async () => {
-		const products = await fetch("/products/all")
-			.then(res =>{console.log(res); return res;}) 
-			.then(res => res.json()); // Process the incoming data
-		console.log(products);
-		setProductsList(products);
-	};
-
-	if(typeof productsList === "object" && productsList !== null){ //eslint-disable-next-line
-		Object.keys(productsList).map((item) => {
-			console.log(productsList[item]);
-			const record = productsList[item];
-			for(const [key, value] of Object.entries(record)) {
-				console.log(`${key}: ${value}`);
-				<p>{`${key}: ${value}`}</p>;
-			}
-		});
-	}
-
-	// useEffect fetches products on initial render only.
-	useEffect(() => {
-		fetchProducts();
-	}, []);
-
+  
 	return (
-		<div className="app">
-			<p>React app render successful, check console logs for current work state.</p>
-		</div>
+		<>
+			<Switch>
+				<Route exact path={"/Authentication"}>
+					<Authentication/>
+				</Route>
+				<Route exact path={"/Inventory"}>
+					<Inventory />
+				</Route>
+			</Switch>
+		</>
 	);
 };
 
