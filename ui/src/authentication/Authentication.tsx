a466235769174b81c4286f50c0dbd325fefa5db8
ElliotRedhead <elliot.redhead@gmail.com>
2020-11-07 16:16:48 +0000

    Added react-bootstrap.

--- ui/src/authentication/Authentication.tsx
+++ ui/src/authentication/Authentication.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const Authentication = () => {
 	
 	return (
-		<p>Authentication contents to go here.</p>
+		<Card>Authentication contents to go here.</Card>
 	);
 };
 
