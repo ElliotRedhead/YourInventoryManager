cbacf70d9c44f16201234c5514bd900a3b1c1f76
ElliotRedhead <elliot.redhead@gmail.com>
2020-11-23 21:41:56 +0000

    Authentication testing react route.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Authentication from "./authentication/Authentication";
 import Inventory from "./inventory/Inventory";
 
+const Authtest = () => {
+	const [auth, setAuth] = useState(null);
+
+	let authMessage = "Authenticated?";
+	if (auth) {
+		authMessage = "You're authenticated!";
+
+	}
+	if (!auth){
+		authMessage = "You're not authenticated!";
+	}
+
+	return (
+		<p>{authMessage}</p>
+	);
+};
 
 const App = () => {
   
@@ -12,8 +28,11 @@ const App = () => {
 				<Route exact path={"/Authentication"}>
 					<Authentication/>
 				</Route>
-				<Route exact path={"/"}>
+				<Route exact path={"/Inventory"}>
 					<Inventory />
+				</Route>
+				<Route exact path={"/Authtest"}>
+					<Authtest />
 				</Route>
 			</Switch>
 		</>
